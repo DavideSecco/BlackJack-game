712077ea8c3fcaaafa4696d3f6bb17d734dc5022
Donato <doniyeah@gmail.com>
2022-05-03 00:55:22 +0200

    Aggiunta la funzione per far giocare il dealer dopo aver cliccato stand

--- src/Panels/ControlPanel.java
+++ src/Panels/ControlPanel.java
@@ -8,15 +8,15 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
-<<<<<<< HEAD
+
 import Main.TestApp;
-=======
+
 import java.util.ArrayList;
 import java.util.List;
 
 import static Main.TestApp.*;
 
->>>>>>> ebb3bab56cb7dd304c47c78a474fb8da2bb5d178
+
 /**
  * E' il pannello dei pulsanti e dei controlli, nella mia testa ci va:
  * - pulsante per chiedere un'altra carta: "hit a card"
@@ -29,18 +29,18 @@ public class ControlPanel extends JPanel implements ActionListener {
     private final JButton standButton;
     private final FichesPanel fichesPanel;
 
-<<<<<<< HEAD
+
 
     private ButtonListener buttonListener;
 
 
 
     public ControlPanel() throws IOException {
-=======
+
     private List<ButtonListener> buttonListeners;
 
     public ControlPanel(Player player, CardsDeck cardsDeck) throws IOException {
->>>>>>> ebb3bab56cb7dd304c47c78a474fb8da2bb5d178
+
         super();
         setPreferredSize(new Dimension(dimension.width, dimension.height/9));
         setLayout(new GridLayout(1,3));
@@ -68,7 +68,7 @@ public class ControlPanel extends JPanel implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == hitButton){
             System.out.println("PULSANTE HIT: sono stato premuto");
-<<<<<<< HEAD
+
             TestApp.player.hitting(TestApp.cardsDeck);
             TestApp.gamePanel.revalidate();
             TestApp.gamePanel.repaint();
@@ -80,7 +80,7 @@ public class ControlPanel extends JPanel implements ActionListener {
             TestApp.dealer.play(TestApp.cardsDeck);
             TestApp.gamePanel.revalidate();
             TestApp.gamePanel.repaint();
-=======
+
             player.addKnownCard(cardsDeck);
             sendToButtonListeners(e);
         }
@@ -92,7 +92,7 @@ public class ControlPanel extends JPanel implements ActionListener {
 
     public void sendToButtonListeners(ActionEvent e){
         for(ButtonListener buttonListener : buttonListeners){
->>>>>>> ebb3bab56cb7dd304c47c78a474fb8da2bb5d178
+
             buttonListener.buttonAction(e);
         }
     }
