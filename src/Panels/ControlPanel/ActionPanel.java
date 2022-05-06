d5838c83450df427c9f3d1be5829363ab9a0aa9b
Donato <doniyeah@gmail.com>
2022-05-06 17:03:01 +0200

    Aggiunti bottoni split e double (Non fanno nulla)

--- src/Panels/ControlPanel/ActionPanel.java
+++ src/Panels/ControlPanel/ActionPanel.java
@@ -12,31 +12,43 @@ public class ActionPanel extends JPanel implements MyPanel{
     public static JButton hitButton;
     public static JButton standButton;
 
+    public static JButton splitButton;
+
+    public static JButton doubleButton;
+
+
     private List<ActionListener> actionListener;
 
     public ActionPanel() {
         super();
-        setLayout(new GridLayout(1,2));
+        setLayout(new GridLayout(1,4));
 
         actionListener = new ArrayList<ActionListener>();
 
         hitButton = new JButton("Hit a Card");
         standButton = new JButton("Stand");
+        doubleButton = new JButton("Double");
+        splitButton = new JButton("Split");
 
         add(hitButton);
         add(standButton);
+        add(doubleButton);
+        add(splitButton);
 
-        hitButton.setEnabled(false);
-        standButton.setEnabled(false);
+        enablePanel(false);
     }
 
     public void enablePanel(boolean bool){
         hitButton.setEnabled(bool);
         standButton.setEnabled(bool);
+        splitButton.setEnabled(bool);
+        doubleButton.setEnabled(bool);
     }
 
     public void addActionListener(ActionListener actionListener) {
         hitButton.addActionListener(actionListener);
         standButton.addActionListener(actionListener);
+        splitButton.addActionListener(actionListener);
+        doubleButton.addActionListener(actionListener);
     }
 }
