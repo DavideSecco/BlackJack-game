f53260ee8052cb9687f83467e8a2579af2eaec5b
Donato <doniyeah@gmail.com>
2022-05-12 17:01:22 +0200

    Aggiunto il bottone per tornare al menu e resa la tabella immodificabile

--- src/Code/Panels/Menu/SelectPlayerPanel.java
+++ src/Code/Panels/Menu/SelectPlayerPanel.java
@@ -5,24 +5,47 @@ import Utils.DBManager;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 import static Code.MyDB.statement;
+import static Code.Panels.MainFrame.mainPanel;
+import static Code.Panels.MainPanel.menuPanel;
 
-public class SelectPlayerPanel extends JPanel {
+public class SelectPlayerPanel extends JPanel implements ActionListener{
+
+    private JButton menu;
+
+    private JPanel parteAlta;
+
+    private List<ActionListener> actionListener;
+
     public SelectPlayerPanel() throws SQLException {
         super();
         setLayout(new BorderLayout());
-        add(BorderLayout.PAGE_START, new JLabel("Ecco tutti i giocatori (sotto questa linea vorrei ci fosse i titoli delle colonne!)"));
+        menu = new JButton("Menu principale");
+        menu.addActionListener(this);
+        parteAlta = new JPanel();
+        parteAlta.setLayout(new GridLayout(1,2));
+        parteAlta.add(new JLabel("Ecco tutti i giocatori (sotto questa linea vorrei ci fosse i titoli delle colonne!)"));
+        parteAlta.add(menu);
+        add(BorderLayout.PAGE_START, parteAlta);
         add(BorderLayout.CENTER, getTable("SELECT * FROM Players"));
     }
 
     public JTable getTable(String query) throws SQLException {
         JTable t = new JTable();
-        DefaultTableModel dm = new DefaultTableModel();
+        DefaultTableModel dm = new DefaultTableModel(){
+            @Override
+            public boolean isCellEditable(int row, int column) {        //serve per rendere inmodificabili le celle
+                return false;
+            }
+        };
 
         // ResultSet rs = DBManager.getConnection().createStatement().executeQuery(query);
         ResultSet rs = statement.executeQuery(query);
@@ -52,5 +75,16 @@ public class SelectPlayerPanel extends JPanel {
         t.setModel(dm);
         t.setGridColor(Color.BLACK);
         return t;
+    }
+
+    public void addActionListener(ActionListener actionListener) {
+        this.actionListener.add(actionListener);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == menu){
+            mainPanel.changePanel(menuPanel);
+        }
     }
 }
